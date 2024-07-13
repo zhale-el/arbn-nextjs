cf83375e596edb8b37ed3662b90242d30555c7a7
zhaleh <zhalehelmi@gmail.com>
2024-07-13 14:49:08 +0330

    add search

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import { BiSearch } from "react-icons/bi";
+
 const Search = () => {
   return (
     <div className="border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer">
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row justify-between items-center">
         <div className="text-sm font-semibold px-6">Anywhere</div>
         <div
           className="hidden sm:block text-sm font-semibold
@@ -11,6 +13,12 @@ const Search = () => {
         "
         >
           Any Week
+        </div>
+        <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
+          <div className="hidden sm:block">Add Guesrs</div>
+          <div className="p-2 bg-rose-500 rounded-full text-white">
+            <BiSearch size={18} />
+          </div>
         </div>
       </div>
     </div>
---
+++ app/components/navbar/UserMenu.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const UserMenu = () => {
+  return (
+    <div className="relative">
+      <div className="flex flex-row items-center gap-3">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default UserMenu;
