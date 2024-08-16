ad2a61ee2236a9f30af81c3ef8c7a163c9c5178b
zhaleh <zhalehelmi@gmail.com>
2024-08-16 19:47:40 +0330

    add button component

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { IconType } from "react-icons";
 
 interface ButtonProps {
@@ -15,9 +17,38 @@ const Button: React.FC<ButtonProps> = ({
   disabled,
   outline,
   small,
-  icon,
+  icon: Icon,
 }) => {
-  return <div>Enter</div>;
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`
+      relative disabled:opacity-70
+      disabled:cursor-not-allowed
+      rounded-lg
+      hover:opacity-80
+      transition w-full
+      ${outline ? "bg-white" : "bg-rose-500"}
+      ${outline ? "border-black" : "border-rose-500"}
+      ${outline ? "text-black" : "text-white"}
+      ${small ? "py-1" : "py-3"}
+      ${small ? "text-sm" : "text-md"}
+      ${small ? "font-light" : "font-semibold"}
+      ${small ? "border-[1px]" : "border-2"}
+      `}
+    >
+      {Icon && (
+        <Icon
+          size={24}
+          className="
+          absolute
+        left-4 top-3"
+        />
+      )}
+      {label}
+    </button>
+  );
 };
 
 export default Button;
